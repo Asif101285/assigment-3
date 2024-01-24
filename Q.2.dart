60f69a83e96a0214195b6547950f127be75c66af
mohammad asif <asifumer974@gmail.com>
2024-01-24 15:02:32 +0500

    Q.2 complete

---
+++ Q.2.dart
@@ -0,0 +1,8 @@
+void main() {
+  // Q.2: Create an empty list of type string called days.
+  // Use the add method to add names of 7 days and print all days.
+
+  List<String> DayNames = [];
+  DayNames.addAll(["monday", "tuesday", "wednesday", "thursday", "friday", "sturday", "sunday"]);
+  print(DayNames);
+}
