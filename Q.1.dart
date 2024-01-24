60234971d615bc6e7526122c68c7b7b74e2a0a9c
mohammad asif <asifumer974@gmail.com>
2024-01-24 14:45:40 +0500

    Q.1 complete

---
+++ Q.1.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Q.1: Create a list of names and print all names using list.
+
+  List<String> name = [
+    "asif",
+    "khizar",
+    "ali",
+    "umer",
+  ];
+  print(name);
+}
