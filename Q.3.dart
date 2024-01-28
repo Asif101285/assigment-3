7995ed1d86482ca26db7763cdd7670ff51bfde06
mohammad asif <asifumer974@gmail.com>
2024-01-28 17:09:25 +0500

    Q.3 complete

--- Q.3.dart
+++ Q.3.dart
@@ -1,6 +1,4 @@
 void main() {
-  
-
   // Q.3: Create a list of Days and remove one by one from the end of list.
 
   List<String> DayNames = [
@@ -12,18 +10,10 @@ void main() {
     "sturday",
     "sunday"
   ];
-  DayNames.removeAt(DayNames.length-1);
-  print(DayNames);
-   DayNames.removeAt(5);
-  print(DayNames);
-   DayNames.removeAt(4);
-  print(DayNames);
-  DayNames.removeAt(3);
-  print(DayNames);
-  DayNames.removeAt(2);
-  print(DayNames);
-  DayNames.removeAt(1);
-  print(DayNames);
-  DayNames.removeAt(0);
-  print(DayNames);
+  int length = DayNames.length;
+  for (var i = 0; i < length; i++) {
+    String removeDay = DayNames.removeLast();
+    print("($removeDay) remove from $DayNames");
+  }
+  print("$DayNames empty List");
 }
---
+++ Q.6.dart
@@ -0,0 +1,32 @@
+void main(){
+  // Q.6: Create Map variable name world then inside it create countries Map, Key
+// will be the name country & country value will have another map having
+// capitalCity, currency and language to it. by using any country key print all the
+// value of Capital & Currency.
+
+
+ Map<String, Map<String, dynamic>> world = {
+    "usa": {
+      "capitalCity": "Washington",
+      "currency": "US Dollar",
+      "language": "English",
+    },
+    "pakistan": {
+      "capitalCity": "islamabad",
+      "currency": "pakistani Rupee",
+      "language": "urdu",
+    },
+    "japan": {
+      "capitalCity": "Tokyo",
+      "currency": "Japanese Yen",
+      "language": "Japanese",
+    },
+    "china":{
+      "capitalCity": "Beijing",
+      "currency": "chinese yuen",
+      "language": "chinese",
+
+    },
+  };
+   print(world, );
+}
