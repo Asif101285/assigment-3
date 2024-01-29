cfa609565c33a1a068669105b1a47cc628b286b8
mohammad asif <asifumer974@gmail.com>
2024-01-29 14:58:03 +0500

    Q.9 complete

--- Q.9.dart
+++ Q.9.dart
@@ -1,3 +1,9 @@
 void main() {
 
+  // Q.9: Given a list of integers, write a dart code that returns the maximum
+  // value from the list.
+  List<int> num = [65, 75, 24, 87, 10];
+  int maxNumber = num.reduce((value, element) => value > element ? value : element);
+  print('The maximum value in the list is: $maxNumber');
+
 }
